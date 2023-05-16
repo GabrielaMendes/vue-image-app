146b0e3fdd145e97a68545d328f53c45a003b153
Gabriela <gdemelo.mendes@gmail.com>
2023-05-16 11:17:56 -0300

    feature: added images to the gallery with lorem picsum

--- src/components/ImageGallery.vue
+++ src/components/ImageGallery.vue
@@ -1,12 +1,13 @@
 <template>
   <v-row>
-    <v-col
-      v-for="n in 12"
-      :key="n"
-      cols="1"
-    >
+    <v-col v-for="n in 200" :key="n" cols="1">
       <v-card>
-        Hello
+        <v-img
+          :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+          aspect-ratio="1"
+          cover
+        >
+        </v-img>
       </v-card>
     </v-col>
   </v-row>
