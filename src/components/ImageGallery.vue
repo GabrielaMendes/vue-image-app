e9ecf59043c0d44ee3c932f0c115be67b34261f4
Gabriela <gdemelo.mendes@gmail.com>
2023-05-16 11:26:50 -0300

    feature: added progress circular as a placeholder to images

--- src/components/ImageGallery.vue
+++ src/components/ImageGallery.vue
@@ -7,6 +7,18 @@
           aspect-ratio="1"
           cover
         >
+          <template v-slot:placeholder>
+            <v-row
+              class="fill-height ma-0"
+              align="center"
+              justify="center"
+            >
+              <v-progress-circular
+                indeterminate
+                color="grey-lighten-5"
+              ></v-progress-circular>
+            </v-row>
+          </template>
         </v-img>
       </v-card>
     </v-col>
