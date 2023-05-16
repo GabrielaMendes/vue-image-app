a8b8b20341aeefaafce9db221f3a527a31b31995
Gabriela <gdemelo.mendes@gmail.com>
2023-05-16 17:34:20 -0300

    feature: added hover effect to pictures

--- src/components/ImageGallery.vue
+++ src/components/ImageGallery.vue
@@ -11,27 +11,29 @@ const { isWithColor } = defineProps({
   <v-card class="mx-5 my-2 pa-3">
     <v-row>
       <v-col v-for="n in 200" :key="n" cols="4" sm="3" md="2" lg="1">
-        <v-card>
-          <v-img
-            :src="`https://picsum.photos/500/300?image=${n * 5 + 10}${
-              isWithColor ? '' : '&grayscale'
-            }`"
-            :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}${
-              isWithColor ? '' : '&grayscale'
-            }`"
-            aspect-ratio="1"
-            cover
-          >
-            <template v-slot:placeholder>
-              <v-row class="fill-height ma-0" align="center" justify="center">
-                <v-progress-circular
-                  indeterminate
-                  color="grey-lighten-5"
-                ></v-progress-circular>
-              </v-row>
-            </template>
-          </v-img>
-        </v-card>
+        <v-hover v-slot="{ isHovering, props }">
+          <v-card :elevation="isHovering ? 12 : 2" v-bind="props">
+            <v-img
+              :src="`https://picsum.photos/500/300?image=${n * 5 + 10}${
+                isWithColor ? '' : '&grayscale'
+              }`"
+              :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}${
+                isWithColor ? '' : '&grayscale'
+              }`"
+              aspect-ratio="1"
+              cover
+            >
+              <template v-slot:placeholder>
+                <v-row class="fill-height ma-0" align="center" justify="center">
+                  <v-progress-circular
+                    indeterminate
+                    color="grey-lighten-5"
+                  ></v-progress-circular>
+                </v-row>
+              </template>
+            </v-img>
+          </v-card>
+        </v-hover>
       </v-col>
     </v-row>
   </v-card>
