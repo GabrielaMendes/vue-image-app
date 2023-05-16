c353b704140e4e036ff7a43ac0a703b01d70d042
Gabriela <gdemelo.mendes@gmail.com>
2023-05-16 10:45:04 -0300

    feature: created sidebar navigation drawer

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
-
+  import TheSideBar from "@/components/TheSideBar.vue"
 </script>
 
 <template>
   <v-app>
     <!-- Sidebar -->
+    <TheSideBar />
     <v-main>
       <!-- Cards -->
     </v-main>
---
+++ src/components/TheSideBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-navigation-drawer :width="280" expand-on-hover rail>
+    <v-list>
+      <v-list-item
+        prepend-avatar="src/assets/profile-pic.png"
+        title="Gabriela Mendes"
+        subtitle="notmyemail@gmail.com"
+      ></v-list-item>
+    </v-list>
+
+    <v-divider></v-divider>
+
+    <v-list density="compact" nav>
+      <v-list-item 
+        prepend-icon="mdi-image"
+        title="Color Gallery"
+        value="color"
+      ></v-list-item>
+      <v-list-item
+        prepend-icon="mdi-dialpad"
+        title="Grayspace Gallery"
+        value="grayspace"
+      ></v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
