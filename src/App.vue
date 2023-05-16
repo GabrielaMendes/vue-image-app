0b05121033ccb0f8a62169f324b2fa7caf623383
Gabriela <gdemelo.mendes@gmail.com>
2023-05-16 17:40:52 -0300

    refactor: changed identation

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script setup>
-  import TheSideBar from "@/components/TheSideBar.vue"
-  import ImageGallery from "@/components/ImageGallery.vue"
-  import { ref } from "vue";
+import TheSideBar from "@/components/TheSideBar.vue"
+import ImageGallery from "@/components/ImageGallery.vue"
+import { ref } from "vue";
 
-  const isWithColor = ref(true);
+const isWithColor = ref(true);
 
-  const onOptionSelected = (isColor) =>  {
-    isWithColor.value = isColor;
-  }
+const onOptionSelected = (isColor) =>  {
+  isWithColor.value = isColor;
+}
 </script>
 
 <template>
